b7dc85f9419c48272af2b6b2a45698223b6f54ff
ranggaazputera <ranggaazputera@gmail.com>
2018-04-02 21:40:10 +0700

    Update loading.js

--- asset/js/loading.js
+++ asset/js/loading.js
@@ -1,3 +1,9 @@
-$(window).load(function(){
-	$("#loading").fadeOut("slow");
-})
+$(window).on("load", function (e) {
+  setTimeout(function(){
+    $('#loading').fadeOut();
+  },1000);
+  setTimeout(function(){
+    $('#loading').remove();
+  },2000);
+
+});
